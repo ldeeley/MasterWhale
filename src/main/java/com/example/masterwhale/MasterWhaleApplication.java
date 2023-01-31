7f6cffbbffbd386b388ed49ccc9188d8b3f3c069
Lester Deeley <lester.deeley@yahoo.com>
2023-01-31 16:22:13 +0000

    Add Dockerfile

---
+++ src/main/java/com/example/masterwhale/MasterWhaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.masterwhale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterWhaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterWhaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/masterwhale/MasterWhaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masterwhale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterWhaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
